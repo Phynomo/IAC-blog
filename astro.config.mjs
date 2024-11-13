247ea876c91ad5a424a502a0602b857c4f7492bb
Daniel Espinoza <123413407+Phynomo@users.noreply.github.com>
2024-11-13 00:09:48 -0600

    UP:Deno

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,12 @@ import { defineConfig, squooshImageService } from "astro/config";
 import remarkCollapse from "remark-collapse";
 import remarkToc from "remark-toc";
 import config from "./src/config/config.json";
+import deno from '@deno/astro-adapter';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: deno(),
   site: config.site.base_url ? config.site.base_url : "http://examplesite.com",
   base: config.site.base_path ? config.site.base_path : "/",
   trailingSlash: config.site.trailing_slash ? "always" : "never",
